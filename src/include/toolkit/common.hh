4d97e59253bf5bda2454f8f84b26fc43f65ce580
Lucas Panian <lucas.panian@gmail.com>
2016-03-29 23:19:21 -0700

    fix incorrect "=delete" for ARRAY_SIZE

--- src/include/toolkit/common.hh
+++ src/include/toolkit/common.hh
@@ -12,7 +12,7 @@
 #define CARRAY_SIZE(ary) (sizeof(ary)/sizeof(ary[0]))
 
 template<class T, int N>
-char (&array_size_helper(T (&)[N]))[N] = delete;
+char (&array_size_helper(T (&)[N]))[N];
 #define ARRAY_SIZE(ary) (sizeof(array_size_helper((ary))))
 
 ////////////////////////////////////////////////////////////////////////////////
