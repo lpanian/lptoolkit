cb964b88dac8de11e3d42c5c21c2933c5de8cd60
Lucas Panian <lucas.panian@gmail.com>
2017-07-19 09:13:10 -0700

    add lptk::StringFromFourCC helper

--- src/include/toolkit/fourcc.hh
+++ src/include/toolkit/fourcc.hh
@@ -17,5 +17,15 @@ namespace lptk
     namespace literals {
         constexpr FourCC operator "" _4cc(const char* s, size_t) { return FourCCFromStr(s); }
     }
+
+    inline void StringFromFourCC(char(&buf)[5], FourCC code)
+    {
+        auto codeint = static_cast<uint32_t>(code);
+        buf[0] = static_cast<char>(codeint & 0xff); codeint >>= 8;
+        buf[1] = static_cast<char>(codeint & 0xff); codeint >>= 8;
+        buf[2] = static_cast<char>(codeint & 0xff); codeint >>= 8;
+        buf[3] = static_cast<char>(codeint & 0xff); 
+        buf[4] = '\0';
+    }
 }
 
