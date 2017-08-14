13fd358a572adb5adf0d761ab144fb4f7ad595c2
Lucas Panian <lucas.panian@gmail.com>
2017-08-14 05:42:14 -0700

    update BitVector primitive and file formatting

--- src/bitvector.cpp
+++ src/bitvector.cpp
@@ -1,108 +1,110 @@
 #include "toolkit/bitvector.hh"
+
 #include <algorithm>
 
 namespace lptk
 {
+    BitVector::BitVector(size_t initialSize, bool initialVal)
+        : m_numBits(initialSize)
+        , m_bytes((initialSize + kNumBits - 1) / kNumBits, PrimType(initialVal ? kMask : 0))
+    {
+    }
 
-BitVector::BitVector(size_t initialSize, bool initialVal)
-	: m_numBits(initialSize)
-	, m_bytes((initialSize+7) / 8, uint8_t(initialVal ? 0xFF : 0))
-{
-}
-	
-BitVector::BitVector(BitVector&& o)
-	: m_numBits(o.m_numBits)
-	, m_bytes(std::move(o.m_bytes))
-{
-	o.m_numBits = 0;
-}
-	
-BitVector::BitVector(const BitVector& o)
-	: m_numBits(o.m_numBits)
-	, m_bytes(o.m_bytes)
-{
-}
-	
-BitVector& BitVector::operator=(BitVector&& o)
-{
-	m_numBits = o.m_numBits;
-	o.m_numBits = 0;
-	m_bytes = std::move(o.m_bytes);
-	return *this;
-}
-	
-BitVector& BitVector::operator=(const BitVector& o)
-{
-	if(this != &o)
-	{
-		m_numBits = o.m_numBits;
-		m_bytes = o.m_bytes;
-	}
-	return *this;
-}
+    BitVector::BitVector(BitVector&& o)
+        : m_numBits(o.m_numBits)
+        , m_bytes(std::move(o.m_bytes))
+    {
+        o.m_numBits = 0;
+    }
 
-void BitVector::set(size_t index, bool value)
-{
-	auto const byteIndex = index >> 3;
-	auto const bitIndex = index - byteIndex * 8;
-	auto const byte = m_bytes[byteIndex];
-	uint8_t const mask = 1 << bitIndex;
-	auto const masked = static_cast<uint8_t>((~mask & byte) | (uint8_t(value) << bitIndex));
-	m_bytes[byteIndex] = masked;
-}
+    BitVector::BitVector(const BitVector& o)
+        : m_numBits(o.m_numBits)
+        , m_bytes(o.m_bytes)
+    {
+    }
 
-bool BitVector::get(size_t index) const
-{
-	auto const byteIndex = index >> 3;
-	auto const bitIndex = index - byteIndex * 8;
-	auto const byte = m_bytes[byteIndex];
-	uint8_t const mask = 1 << bitIndex;
-	return 0 != (mask & byte);
-}
+    BitVector& BitVector::operator=(BitVector&& o)
+    {
+        if (this != &o)
+        {
+            m_numBits = o.m_numBits;
+            o.m_numBits = 0;
+            m_bytes = std::move(o.m_bytes);
+        }
+        return *this;
+    }
 
-void BitVector::resize(size_t newSize, bool value)
-{
-	const uint8_t newDataVal = value ? 0xFF : 0x00;
-	const auto numBytes = (newSize + 7) / 8;
-	m_bytes.resize(numBytes, newDataVal);
+    BitVector& BitVector::operator=(const BitVector& o)
+    {
+        if (this != &o)
+        {
+            m_numBits = o.m_numBits;
+            m_bytes = o.m_bytes;
+        }
+        return *this;
+    }
 
-	// copy the partial byte
-	const auto numWholeBits = 8 * (m_numBits / 8);
-	const auto numPartialBits = m_numBits - numWholeBits;
-	const auto numNewPartialBits = newSize - numWholeBits;
-	if(numPartialBits > 0) 
-	{
-		const uint8_t partialByte = m_bytes[numBytes - 1];
-		const uint8_t oldMask = (1 << numPartialBits) - 1;
-		// extra mask with 0xff to get rid of VS exception about losing data. It claims
-		// the resulting code won't be changed...
-		const uint8_t newMask = 0xFF & (~oldMask & ((1 << numNewPartialBits) - 1)); 
-		const uint8_t newByte = (partialByte & oldMask) | (newDataVal & newMask);
-		m_bytes[numBytes - 1] = newByte;
-	}
+    void BitVector::set(size_t index, bool value)
+    {
+        auto const byteIndex = index >> kLog2;
+        auto const bitIndex = index - byteIndex * kNumBits;
+        auto const byte = m_bytes[byteIndex];
+        PrimType const mask = PrimType(1) << bitIndex;
+        auto const masked = static_cast<PrimType>((~mask & byte) | (PrimType(value) << bitIndex));
+        m_bytes[byteIndex] = masked;
+    }
 
-	m_numBits = newSize;
-}
+    bool BitVector::get(size_t index) const
+    {
+        auto const byteIndex = index >> kLog2;
+        auto const bitIndex = index - byteIndex * kNumBits;
+        auto const byte = m_bytes[byteIndex];
+        PrimType const mask = PrimType(1) << bitIndex;
+        return 0 != (mask & byte);
+    }
 
-void BitVector::push_back(bool value)
-{
-	++m_numBits;
-	if( (m_numBits+7) / 8 > m_bytes.size())
-		m_bytes.push_back(0);
-	set(m_numBits - 1, value);
-}
+    void BitVector::resize(size_t newSize, bool value)
+    {
+        const PrimType newDataVal = value ? kMask : 0x00;
+        const auto numBytes = (newSize + kNumBits - 1) / kNumBits;
+        m_bytes.resize(numBytes, newDataVal);
 
-bool BitVector::operator[](size_t index) const
-{
-	return get(index);
-}
+        // copy the partial byte
+        const auto numWholeBits = kNumBits * (m_numBits / kNumBits);
+        const auto numPartialBits = m_numBits - numWholeBits;
+        const auto numNewPartialBits = newSize - numWholeBits;
+        if (numPartialBits > 0)
+        {
+            const PrimType partialByte = m_bytes[numBytes - 1];
+            const PrimType oldMask = (1 << numPartialBits) - 1;
+            // extra mask with 0xff to get rid of VS exception about losing data. It claims
+            // the resulting code won't be changed...
+            const PrimType newMask = kMask & (~oldMask & ((1 << numNewPartialBits) - 1));
+            const PrimType newByte = (partialByte & oldMask) | (newDataVal & newMask);
+            m_bytes[numBytes - 1] = newByte;
+        }
 
-void BitVector::swap(BitVector& other)
-{
-	std::swap(m_numBits, other.m_numBits);
-	m_bytes.swap(other.m_bytes);
-}
+        m_numBits = newSize;
+    }
 
+    void BitVector::push_back(bool value)
+    {
+        ++m_numBits;
+        if ((m_numBits + kNumBits - 1) / kNumBits > m_bytes.size())
+            m_bytes.push_back(0);
+        set(m_numBits - 1, value);
+    }
+
+    bool BitVector::operator[](size_t index) const
+    {
+        return get(index);
+    }
+
+    void BitVector::swap(BitVector& other)
+    {
+        std::swap(m_numBits, other.m_numBits);
+        m_bytes.swap(other.m_bytes);
+    }
 }
 
 
--- src/include/toolkit/bitvector.hh
+++ src/include/toolkit/bitvector.hh
@@ -2,34 +2,41 @@
 #ifndef INCLUDED_toolkit_bitvector_HH
 #define INCLUDED_toolkit_bitvector_HH
 
-#include "dynary.hh"
 #include <cstdint>
 
-namespace lptk {
+#include "dynary.hh"
 
-class BitVector
+namespace lptk 
 {
-public:
-	BitVector(size_t initialSize = 0, bool initialVal = false);
-	BitVector(BitVector&& o);
-	BitVector(const BitVector& o);
-	BitVector& operator=(const BitVector& o);
-	BitVector& operator=(BitVector&& o);
 
-	void set(size_t index, bool value);
-	bool get(size_t index) const;
+    class BitVector
+    {
+        using PrimType = uint64_t;
+        static constexpr auto kNumBits = sizeof(PrimType) * 8;
+        static constexpr auto kLog2 = 6;
+        static constexpr auto kMask = ~PrimType(0);
 
-	size_t size() const { return m_numBits; }
-	void resize(size_t newSize, bool value = false);
+    public:
+        BitVector(size_t initialSize = 0, bool initialVal = false);
+        BitVector(BitVector&& o);
+        BitVector(const BitVector& o);
+        BitVector& operator=(const BitVector& o);
+        BitVector& operator=(BitVector&& o);
 
-	void push_back(bool value);
+        void set(size_t index, bool value);
+        bool get(size_t index) const;
 
-	bool operator[](size_t index) const;
-	void swap(BitVector& other);
-private:
-	size_t m_numBits;
-	DynAry<uint8_t> m_bytes;
-};
+        size_t size() const { return m_numBits; }
+        void resize(size_t newSize, bool value = false);
+
+        void push_back(bool value);
+
+        bool operator[](size_t index) const;
+        void swap(BitVector& other);
+    private:
+        size_t m_numBits;
+        DynAry<PrimType> m_bytes;
+    };
 
 }
 
