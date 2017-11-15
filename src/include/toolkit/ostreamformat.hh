10fe573ab70fb9795c937870da5cccd369441ae9
Lucas Panian <lucas.panian@gmail.com>
2017-11-15 09:56:03 -0800

    add ostreamformat.hh for ostream helpers

---
+++ src/include/toolkit/ostreamformat.hh
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef INCLUDED_toolkit_ostreamformat_HH
+#define INCLUDED_toolkit_ostreamformat_HH
+
+#include <ostream>
+
+namespace lptk
+{
+    // silly ostream format helpers
+    namespace ostreamformat
+    {
+        struct spaces
+        {
+            int count;
+            explicit spaces(int c) : count(4 * c) {}
+        };
+
+        std::ostream& operator<<(std::ostream& out, const spaces& s)
+        {
+            for (int i = 0; i < s.count; ++i)
+                out << " ";
+            return out;
+        }
+    }
+}
+
+#endif
+
